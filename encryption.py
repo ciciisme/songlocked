2083551462f6268a2923a0a154708f59c204c379
Cibo <bautista.cibo@gmail.com>
2024-06-04 15:31:38 +0800

    made the frame work

--- encryption.py
+++ encryption.py
@@ -1,10 +1,7 @@
-import os
 from dotenv import load_dotenv
-import base64
-from requests import post, get
 import json
 from random import shuffle
-from spotipy import get_token, auth_head, search_song_items, artist_list
+import framework
 # finables, ( end-trans, start-trans)
 #ask if you want to run code 1 or code 2
 done = 0
@@ -14,44 +11,16 @@ with open("D:\solocoding\python studi\songlocked\cipher.json") as file:
 
 load_dotenv("D:\solocoding\python studi\songlocked\.env")
 
-token = get_token()
-
 q1 = "Hello I am the song encryptor \n I will encrypt your password based on your favorite song \n Encrypt or Decrypt password?"
 print(q1)
 
+questions = framework.questions("ENCRYPT")
 
-while True:
- choice = input()
- if choice.upper() == str("ENCRYPT"):
-   #find start-trans
-   print("what is your favorite song")
-   favtemp = str(input())
-   favitems = str(search_song_items(favtemp))
-   break
-
-
-while len(favitems) == 0:
-   print("The system cannot find your song")
-   favtemp = input("Please try again: ")
-   favitems = str(search_song_items(favtemp))
-   
-
-print(f"which of these artists made your favorite song \n {artist_list(favtemp)} \n (if there is a duplicate, choose the lowest number)")
-favartist = input("Place artist number: ")
-print("If there was a typo in the title of the song, the encryption will be wrong \
-      \n ---------------------------------------------------------------------------")
-while True:
-   try:
-     favartist = int(favartist)
-     if favartist < 6 and favartist > 0:
-      break
-     else:
-      continue
-   except:
-      input("Place artist number under 5: ")
-
-favsong = str(search_song_items(favtemp)[int(favartist)]["name"])
-favsongid = str(search_song_items(favtemp)[int(favartist)]["id"])
+favartist = questions[0]
+favtemp = questions[1]
+favitems = questions[2]
+favsong = questions[3]
+favsongid = questions[4]
 password = input("Place the password you want to encrypt: ")
 password = str(password)
 
---
+++ framework.py
@@ -0,0 +1,41 @@
+from spotipy import search_song_items, artist_list
+
+
+def questions(mode):
+    while True:
+        choice = input()
+        if choice.upper() == str(mode):
+        #find start-trans
+            print("what is your favorite song")
+        favtemp = str(input())
+        favitems = str(search_song_items(favtemp))
+        break
+
+
+    while len(favitems) == 0:
+        print("The system cannot find your song")
+    favtemp = input("Please try again: ")
+    favitems = str(search_song_items(favtemp))
+    
+
+    print(f"which of these artists made your favorite song \n {artist_list(favtemp)} \n (if there is a duplicate, choose the lowest number)")
+    favartist = input("Place artist number: ")
+    print("If there was a typo in the title of the song, the encryption will be wrong \
+        \n ---------------------------------------------------------------------------")
+    while True:
+        try:
+            favartist = int(favartist)
+            if favartist < 6 and favartist > 0:
+                break
+            else:
+                continue
+        except:
+            input("Place artist number under 5: ")
+
+    favsong = str(search_song_items(favtemp)[int(favartist)]["name"])
+    favsongid = str(search_song_items(favtemp)[int(favartist)]["id"])
+    
+    return favartist, favtemp, favitems, favsong, favsongid
+
+
+      
